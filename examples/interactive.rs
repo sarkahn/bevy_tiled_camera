b957e4499b2b68ff14a8b4dea8a3272bde1d7c6e
sark <sarkahn@gmail.com>
2021-11-21 14:14:19 -0700

    Clippy,fmt

--- examples/interactive.rs
+++ examples/interactive.rs
@@ -1,9 +1,9 @@
-// /// A simple interactive demo. Resize the window to see the viewport auto-adjust.
-// ///
-// /// # Controls:
-// /// Spacebar - Toggle camera between centered or bottom-left origin
-// /// Arrow Keys - Adjust the number of tiles
-// /// Tab - Change the current tile textures
+/// A simple interactive demo. Resize the window to see the viewport auto-adjust.
+///
+/// # Controls:
+/// Spacebar - Toggle camera between centered or bottom-left origin
+/// Arrow Keys - Adjust the number of tiles
+/// Tab - Change the current tile textures
 
 use bevy::prelude::*;
 use bevy_tiled_camera::{TiledCameraBundle, TiledCameraPlugin, TiledProjection};
